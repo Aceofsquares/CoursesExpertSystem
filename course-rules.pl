e5182c35cee6cb053c756e9b2c7ffd81e2ec1e82
Brian Rogers <btcr1990@gmail.com>
2018-10-17 02:51:20 -0500

    Removed need for the permutation.

--- course-rules.pl
+++ course-rules.pl
@@ -1,21 +1,10 @@
-%Rules for corequisites
-is_a_coreq(Cours, Coreq):-coreqs(Course, Coreqs),member(Coreq,Coreqs),!.
+%%Rules for corequisites
+is_a_coreq(Course, Coreq):-coreqs(Course, Coreqs),member(Coreq,Coreqs),!.
 
 %Rules for prerequisites
 is_a_prereq(Course, Prereq):-prereqs(Course, Prereqs),member(Prereq,Prereqs),!.
 has_prereqs(Course):-prereqs(Course, _).
 
-%what if all courses are passed in?  Needs redone
-has_all_prereqs(Course, Prereqs):-perm(Prereqs,Result),prereqs(Course, Result),!.
-
-%Utilty Rules
-
-%------------------Permutation of Lists----------------------------------------------
-%Found at http://kti.ms.mff.cuni.cz/~bartak/prolog/combinatorics.html
-%Will be changed so this is not needed.
-perm(List,[H|Perm]):-delete(H,List,Rest),perm(Rest,Perm).
-perm([],[]).
-   
-delete(X,[X|T],T).
-delete(X,[H|T],[H|NT]):-delete(X,T,NT).
-%-------------------------------------------------------------------------------------
+has_all_prereqs(Course, Taken):-prereqs(Course,Required), has_all_prereqs(Course, Taken, Required).
+has_all_prereqs(Course, Taken, [Prereq|Rest]):-member(Prereq,Taken),has_all_prereqs(Course,Taken,Rest),!.
+has_all_prereqs(_, _, []).
